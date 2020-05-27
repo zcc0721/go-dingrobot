2e09b59707e0ebef891d36e41d3f265c66864ab8
Chengcheng Zhang <943420582@qq.com>
2020-05-27 19:19:00 +0800

    update

--- example.go
+++ example.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// More details about ding talk doc: https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq/9e91d73c
 func Example() {
 	webHook := "https://oapi.dingtalk.com/robot/send?access_token=5f6585f2ed6b9461edb4391ff9a8a128a66643ba082054e90a5d1cdef547f53a"
 	atMobiles := []string{"18612345678"}
